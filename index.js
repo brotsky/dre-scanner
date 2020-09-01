9c333e2a6831b8b4ce4a20cf4b7a422dd3fd6734
brotsky <brandon@brotskydesigns.com>
2020-08-31 19:24:14 -0700

    ..

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ const axios = require('axios');
 const express = require('express')
 const app = express()
 
-
-axios.post('https://secure.dre.ca.gov/PublicASP/CurrentExams.asp', {
+const body = {
   cb_examSites: [
-    "'Fresno'",
-    "'Los Angeles'",
-    "'SF/Oakland'",
-    "'Sacramento'",
-    "'San Diego'",
+    "%27Fresno%27",
+    "%27Los Angeles%27",
+    "%27SF/Oakland%27",
+    "%27Sacramento%27",
+    "%27San Diego%27",
   ],
-  cb_examTypes: "'Salesperson'",
+  cb_examTypes: "%27Salesperson%27",
   B1: "Get Exam List",
   action : "GO"
-}).then(response => {
-  console.log('response', response);
-})
+};
 
-module.exports = (req, res) => {
-  const { name = 'World' } = req.query
-  res.send(`Hello ${name}!`)
-}
+
+app.get('/', async (req, res) => {
+  const site = await axios({
+    method: 'POST',
+    url: 'https://secure.dre.ca.gov/PublicASP/CurrentExams.asp',
+    data: body,
+    headers: {'Content-Type': 'multipart/form-data' }
+  });
+  res.send(site.data)
+})
+ 
+app.listen(process.env.PORT || 3000)
